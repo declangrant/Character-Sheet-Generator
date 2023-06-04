1c58fe6fb4c8f7efc4c63506e89b1cd221e642ae
Declan Grant <declangrant612@gmail.com>
2023-06-03 22:20:42 -0600

    add comments

--- character_sheet_generator.py
+++ character_sheet_generator.py
@@ -8,49 +8,71 @@ import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning) 
 
 class CharacterSheetGenerator:
+    """
+    Class to generate a Cyberpunk 2020 character sheet with selected skills.
+    """
 
     SKILLS_SCHEMA = {
-        "skill": str,
-        "stat": str,
-        "count": int,
-        "entry": bool,
-        "enabled": bool,
-        "source": str
+        "skill": str,       # name of the skill
+        "stat": str,        # name of the stat, or 'special abilities'
+        "count": int,       # number of occurences, 0 if only one is allowed
+        "entry": bool,      # whether to add a solid line for a player to write
+        "enabled": bool,    # whether the skill will be included
+        "source": str       # what sourcebook the skill is defined in, or custom
     }
 
-    def __init__(self, data_dir: str):
+    def __init__(self):
+        """
+        Initiates a new CharacterSheetGenerator.
+        """
 
-        self.front_img_path = f"{data_dir}/front.png"
-        self.back_img_path = f"{data_dir}/back.png"
+        # template image files
+        self.front_img_path = "cp2020/front.png"
+        self.back_img_path = "cp2020/back.png"
 
-        with open(f"{data_dir}/text_info.json", "r") as file:
+        # dict with information about text placement
+        with open("cp2020/text_info.json", "r") as file:
             self.text_info = json.load(file)
 
-        with open(f"{data_dir}/skills.csv", "r") as file:
+        # list of skills with extra data
+        with open("cp2020/skills.csv", "r") as file:
             reader = csv.DictReader(file)
             self.skills = []
             for row in reader:
                 self.skills.append({k:v if self.SKILLS_SCHEMA[k]==str else ast.literal_eval(v) for k,v in row.items()})
 
+        # load fonts
         self.stat_font = ImageFont.truetype(self.text_info["stat_font"]["file"], self.text_info["stat_font"]["size"])
         self.skill_font = ImageFont.truetype(self.text_info["skill_font"]["file"], self.text_info["skill_font"]["size"])
 
+        # define symbols to add to the skill text
         self.fill_symbol = "[       ]"
         self.fill_width = self.skill_font.getsize(self.fill_symbol)[0]
         self.dot_char_width = self.skill_font.getsize(".")[0]
         self.entry_char_width = self.skill_font.getsize("_")[0]
 
 
-    def _add_row(self, text: str, skill: bool, entry: bool = False):
+    def _add_row(self, text: str, skill: bool, entry: bool = False) -> None:
+        """
+        Adds a new header or skill to the page.
 
+        Args:
+            text: name of skill or header
+            skill: whether or not the item is a skill
+            entry: whether or not the skill should have a line for custom entry
+        """
+
+        # use box's override width if defined, otherwise default
         if "override_width" in self.text_info["boxes"][self.box_num]:
             col_width = self.text_info["boxes"][self.box_num]["override_width"]
         else:
             col_width = self.text_info["column_width"]
 
+        # position of the line
         x_text = self.text_info["boxes"][self.box_num]["x"]
         y = self.text_info["boxes"][self.box_num]["y"]+self.row_num*self.text_info["row_height"]
 
+        # add lines and space for skill value
         if skill:
             width = self.skill_font.getsize(text)[0] + self.fill_width
             if entry:
@@ -64,6 +86,7 @@ class CharacterSheetGenerator:
 
         self.text_layer.text((x_text,y), text, "black", font=self.skill_font if skill else self.stat_font)
 
+        # go to next row or box
         if self.row_num < self.text_info["boxes"][self.box_num]["rows"]-1:
             self.row_num += 1
         else:
@@ -72,6 +95,9 @@ class CharacterSheetGenerator:
 
 
     def execute(self):
+        """
+        Generates the PDF file.
+        """
 
         front_img = Image.open(self.front_img_path).convert("RGB")
         self.text_layer = ImageDraw.Draw(front_img)
@@ -105,5 +131,5 @@ class CharacterSheetGenerator:
 
 
 if __name__ == "__main__":
-    generator = CharacterSheetGenerator("cp2020")
+    generator = CharacterSheetGenerator()
     generator.execute()
