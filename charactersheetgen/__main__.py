1f9f0df51b5a4d56db945f5209e46f5bb1f96df0
Declan Grant <declangrant612@gmail.com>
2023-06-05 20:28:29 -0600

    handle calling program from other dirs

--- charactersheetgen/__main__.py
+++ charactersheetgen/__main__.py
@@ -1,11 +1,19 @@
-import sys
-from PyQt5 import QtWidgets
-
-from charactersheetgen.app_widget import AppWidget
-
-
 if __name__ == "__main__":
 
+    import sys
+    import os
+    from PyQt5 import QtWidgets
+
+    # change to project's root directory if not already there
+    root_dir = __file__.rsplit(os.sep, 2)[0]
+    if os.getcwd() != root_dir:
+        os.chdir(root_dir)
+    if root_dir not in sys.path:
+        sys.path.append(root_dir)
+
+    from charactersheetgen.app_widget import AppWidget
+
+    # run the app
     app = QtWidgets.QApplication([])
 
     widget = AppWidget()
